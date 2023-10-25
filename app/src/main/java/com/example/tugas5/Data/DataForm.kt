5cf06788f8fc094ecd69589eb3a7265716d1d173
Richi Fendrika <ASUS@LAPTOP-18FMB87U>
2023-10-25 19:19:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas5/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.tugas5.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/tugas5/Data/DataSource.kt
@@ -0,0 +1,9 @@
+package com.example.tugas5.Data
+import com.example.tugas5.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
